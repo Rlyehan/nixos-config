872f8883767f2b9df7d1fcdcb5782184a413054d
Maximilian Huber <max_hub@gmx.net>
2023-11-16 14:04:19 +0100

    refactor of home folder

--- hosts/default.nix
+++ hosts/default.nix
@@ -16,8 +16,6 @@
   ];
 
 
-  # Enable binfmt emulation of aarch64-linux, this is required for cross compilation.
-  boot.binfmt.emulatedSystems = ["aarch64-linux" "riscv64-linux"];
   # supported fil systems, so we can mount any removable disks with these filesystems
   boot.supportedFilesystems = [
     "ext4"
