1f139028b388eb4cec42057ad004c6cf346e0a0b
Maximilian Huber <max_hub@gmx.net>
2023-11-20 17:33:57 +0100

    cleaning up virt

--- modules/libvirt.nix
+++ modules/libvirt.nix
@@ -3,17 +3,10 @@
   pkgs,
   ...
 }: {
-  ###################################################################################
-  #
-  #  Enable Libvirt(QEMU/KVM), install qemu-system-riscv64/qemu-system-loongarch64/...)
-  #
-  ###################################################################################
 
   virtualisation = {
     libvirtd = {
       enable = true;
-      # hanging this option to false may cause file permission issues for existing guests.
-      # To fix these, manually change ownership of affected files in /var/lib/libvirt/qemu to qemu-libvirtd.
       qemu.runAsRoot = true;
     };
   };
@@ -27,19 +20,10 @@
     #   qemu-pr-helper qemu-nbd elf2dmp qemu-img qemu-io
     #   qemu-kvm qemu-system-x86_64 qemu-system-aarch64 qemu-system-i386
     qemu_kvm
-
-    # Install all packages about QEMU, provides:
-    #   ......
-    #   qemu-loongarch64 qemu-system-loongarch64
-    #   qemu-riscv64 qemu-system-riscv64 qemu-riscv32  qemu-system-riscv32
-    #   qemu-system-arm qemu-arm qemu-armeb qemu-system-aarch64 qemu-aarch64 qemu-aarch64_be
-    #   qemu-system-xtensa qemu-xtensa qemu-system-xtensaeb qemu-xtensaeb
-    #   ......
     qemu_full
   ];
 
-  boot.kernelModules = ["kvm-amd" "kvm-intel"];
+  boot.kernelModules = "kvm-amd";
   # Enable nested virsualization, required by security containers and nested vm.
-  boot.extraModprobeConfig = "options kvm_intel nested=1"; # for intel cpu
-  # boot.extraModprobeConfig = "options kvm_amd nested=1";  # for amd cpu
+  boot.extraModprobeConfig = "options kvm_amd nested=1";  # for amd cpu
 }
