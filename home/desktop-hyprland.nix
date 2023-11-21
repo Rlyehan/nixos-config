ecb046a77e61710647283be575562f5b3654cb3c
Maximilian Huber <max_hub@gmx.net>
2023-11-21 20:30:01 +0100

    working on hypr

--- home/desktop-hyprland.nix
+++ home/desktop-hyprland.nix
@@ -7,19 +7,10 @@
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home = {
-    username = "max";
-    homeDirectory = "/home/max";
+    username = username;
+    homeDirectory = "/home/${username}";
 
     stateVersion = "23.05";
-  };
-
-    # allow fontconfig to discover fonts and configurations installed through home.packages
-  fonts.fontconfig.enable = true;
-
-  systemd.user.sessionVariables = {
-    "NIXOS_OZONE_WL" = "1"; # for any ozone-based browser & electron apps to run on wayland
-    "MOZ_ENABLE_WAYLAND" = "1"; # for firefox to run on wayland
-    "MOZ_WEBRENDER" = "1";
   };
 
   # Let Home Manager install and manage itself.
