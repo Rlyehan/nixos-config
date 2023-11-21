f741234ab307a02d843705f9984a99effa83b193
Maximilian Huber <max_hub@gmx.net>
2023-11-21 20:18:34 +0100

    trying to fix hypr

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,13 +1,19 @@
-{ inputs, pkgs, lib, config, ... }:
+{pkgs, hyprland, catppuccin-hyprland, ...}: {
 
-with lib;
-let cfg = config.modules.hyprland;
-
-in {
-    config = mkIf cfg.enable {
-        home.file.".config/hypr" = {
+  # hyprland configs, based on https://github.com/notwidow/hyprland
+  home.file.".config/hypr" = {
     source = ./hypr-conf;
+    # copy the scripts directory recursively
     recursive = true;
-    };
-    };
+  };
+
+  # allow fontconfig to discover fonts and configurations installed through home.packages
+  fonts.fontconfig.enable = true;
+
+  systemd.user.sessionVariables = {
+    "NIXOS_OZONE_WL" = "1"; # for any ozone-based browser & electron apps to run on wayland
+    "MOZ_ENABLE_WAYLAND" = "1"; # for firefox to run on wayland
+    "MOZ_WEBRENDER" = "1";
+  };
+
 }
