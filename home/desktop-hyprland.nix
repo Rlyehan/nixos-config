b1d49b6af2152a9b26d472f4d5515575f2e38c85
Maximilian Huber <max_hub@gmx.net>
2023-11-21 19:47:22 +0100

    next refactor attempt

--- home/desktop-hyprland.nix
+++ home/desktop-hyprland.nix
@@ -1,4 +1,4 @@
-{ username, ... }: {
+{ username, hyprland, ... }: {
   imports = [
     ./programs
     ./hyprland
@@ -7,18 +7,25 @@
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home = {
-    username = username;
-    homeDirectory = "/home/${username}";
+    username = "max";
+    homeDirectory = "/home/max";
 
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
+    file.".config/hypr" = {
+    source = ./hypr-conf;
+    # copy the scripts directory recursively
+    recursive = true;
+    };
+
     stateVersion = "23.05";
+  };
+
+    # allow fontconfig to discover fonts and configurations installed through home.packages
+  fonts.fontconfig.enable = true;
+
+  systemd.user.sessionVariables = {
+    "NIXOS_OZONE_WL" = "1"; # for any ozone-based browser & electron apps to run on wayland
+    "MOZ_ENABLE_WAYLAND" = "1"; # for firefox to run on wayland
+    "MOZ_WEBRENDER" = "1";
   };
 
   # Let Home Manager install and manage itself.
